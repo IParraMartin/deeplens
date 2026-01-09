66d7975b9228e6aa86e5491a7cbb6c1ddf236f02
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2026-01-08 23:33:10 -0800

    Update model configurations: change default Hugging Face model to 'microsoft/phi-2' and adjust sample size and sequence length; expand supported models list in documentation

--- demo/extract.py
+++ demo/extract.py
@@ -1,11 +1,11 @@
 from deeplens.extractor import FromHuggingFace
 
 extractor = FromHuggingFace(
-    hf_model="gpt2-medium",
+    hf_model="microsoft/phi-2",
     layer=3,
     dataset_name="HuggingFaceFW/fineweb",
-    num_samples=2500,
-    seq_length=1024,
+    num_samples=100,
+    seq_length=128,
     inference_batch_size=16,
     device="auto",
     save_features=True
